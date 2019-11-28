096e603a16e08a84e04511f2627adb0ff12e85fd
clair5610 <58032072+clair5610@users.noreply.github.com>
2019-11-28 21:12:28 +0800

    20191128 點擊開始

--- UnityProject/Assets/Scripts/LearnOprator.cs
+++ UnityProject/Assets/Scripts/LearnOprator.cs
@@ -3,7 +3,13 @@
 public class LearnOprator : MonoBehaviour
 {
     public int a = 10, b = 3;
-    public int c = 9, d = 1;  
+    public int c = 9, d = 1;
+
+    public int apple = 5;
+    public int coin = 3;
+
+    public int key = 1;
+    public int boom = 2;
 
     private void Start()
     {
@@ -30,5 +36,33 @@ public class LearnOprator : MonoBehaviour
         print(c <= d);  // false
         print(c == d);  // false
         print(c != d);  // true
+
+        // 邏輯運算子
+        // 結果是布林值
+        // && = 意思是並且
+        // 只要有一個 false (否) 就會傳回 false
+        print(true && true);    //true
+        print(true && false);   //false
+        print(false && true);   //false
+        print(false && false);  //false
+
+        // ||(shift+\打出來的) = 或者的意思
+        // 規則 : 只要有一個 true 就會傳回 true
+        print(true || true);    //true
+        print(true || false);   //true
+        print(false || true);   //true
+        print(false || false);  //false
+
+        // 相反 ( 顛倒 ) ! (驚歎號要放在(後面喔
+        // 作用 : 將布林值顛倒
+        // ex:可以用在遊戲中關門開門
+        print(!true);
+        print(!false);
+
+        // 任務：取得十個蘋果和五個金幣
+        print(apple >= 10 && coin >= 5);
+
+        // 任務 : 取得一個鑰匙或者兩個炸彈
+        print(key >=1 && boom >= 2);
     }
 }
---
+++ UnityProject/Assets/Scripts/Learnif.cs
@@ -0,0 +1,71 @@
+﻿
+using UnityEngine;
+
+public class Learnif : MonoBehaviour
+{
+    public bool open;
+    public int score;
+    public int combo;
+
+    private void Start()
+    {
+        // 判斷式 if (switch)
+        // 語法 :
+        //if (布林值) { 當if () 布林值為 true 執行這裡}
+
+        if (true)
+        {
+            print("嗨~");
+        }  
+
+    }
+
+    private void Update()
+    {
+        // if () 布林值為 true 會執行 if {}
+        if (open)
+        {
+            print("開門!");
+        }
+        // if () 布林值為 false 會執行 else {} (否則)
+        else
+        {
+            print("關門!");    
+        }
+
+        if (score >= 60)
+        {
+            print("及格了!");
+        }
+        else if (score >= 40)
+        {
+            print("你可以補考!");
+        }
+        else if (score >= 40)
+        {
+            print("你可以付錢補考!");
+        }
+        else
+        {
+            print("不及格!");   
+        }
+
+        //連擊判斷
+        //數值大的先寫，如果數值小在上程序判斷完之後會直接跳出{}外，
+        //那麼之後數值大的便不會判斷到，另外一種方法是使用 && <=
+
+        if (combo >= 150)
+        {
+            print("攻擊10倍");
+        }
+        else if (combo >= 100)
+        {
+            print("攻擊5倍");
+        }
+        else if (combo >= 50)
+        {
+            print("攻擊2倍");
+        }
+
+    }
+}
